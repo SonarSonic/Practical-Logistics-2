efa54396692194e277f9f63cac720ce2bb9e00bf
SonarSonic <ollielansdell@hotmail.co.uk>
2018-05-11 00:12:14 +0100

    Updated Gradle and Auto-Versioning

--- src/main/java/sonar/logistics/PL2Constants.java
+++ src/main/java/sonar/logistics/PL2Constants.java
@@ -4,8 +4,8 @@ public class PL2Constants {
 
 	public static final String MODID = "practicallogistics2";
 	public static final String NAME = "Practical Logistics 2";
-	public static final String VERSION = "3.0.4";
-	public static final String SONAR_CORE = "5.0.9";
-	public static final String MCMULTIPART = "2.5.0";
+	public static final String VERSION = "@VERSION@";
+	public static final String SONAR_CORE = "@SONAR_VERSION@";
+	public static final String MCMULTIPART = "@MCMP_VERSION@";
 	public static final String MINECRAFT = "minecraft";
 }
